689323eb7b12ceee8ff6f06ed2efe341d00f1812
Ruhul <itsruhul@proton.me>
2026-06-08 01:28:46 +0530

    learn module.exports

---
+++ math.js
@@ -0,0 +1,13 @@
+//module.exports.sum = (a,b) => a+b;
+const mul = (a,b) => a*b;
+const g = 9.8;
+const PI = 3.14;
+
+module.exports = {
+  sum: sum,
+  mul: mul,
+  g: g,
+  PI: PI,
+};
+
+//module.exports = obj;
---
+++ script.js
@@ -0,0 +1,14 @@
+// let n = 5;
+
+// for(let i = 0; i < n; i++){
+//   console.log("Hello", i);
+// }
+
+// let args = process.argv;
+// for(let i = 2; i < args.length; i++ ){
+//   console.log("Hello", args[i]);
+// }
+
+const someValue = require("./math");
+
+console.log(someValue);
