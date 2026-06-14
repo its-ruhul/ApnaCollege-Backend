75a3b6e6508c2e8acb12549842e27b013d5e5b80
Ruhul <itsruhul@proton.me>
2026-06-14 22:27:22 +0530

    learn sending request and install nodemon

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -9,8 +9,32 @@ app.listen(port, () => {
   console.log(`App is listening on port ${port}`)
 });
 
-app.use((req, res) => {
-  // console.log(req);
-  console.log("request received");
-  res.send("<h1>Fruits</h1> <ul><li>Banana</li><li>Apple</li></ul>");
+app.get("/", (req, res) => {
+  res.send("You contacted root path");
 });
+
+app.get("/apple", (req, res) => {
+  res.send("You contacted apple path");
+});
+
+app.get("/orange", (req, res) => {
+  res.send("You contacted orange path");
+});
+
+app.use((req, res) => {
+  res.status(404).send("ERROR 404: This page doesn't exist.");
+});
+
+app.post("/", (req, res) => {
+  res.send("You sent a post request to root");
+})
+
+// app.get("*", (req, res) => {
+//   res.send("This path doesn't exist");
+// });
+
+// app.use((req, res) => {
+//   // console.log(req);
+//   console.log("request received");
+//   res.send("<h1>Fruits</h1> <ul><li>Banana</li><li>Apple</li></ul>");
+// });
